f4887187e3d2d6517553c49d66889fcb342bb553
DataWorld Campus <dataworldcampus@gmail.com>
2019-12-09 13:07:20 -0800

    Volcado de la salida a un dir out específico para cada aplicación

--- datawarehouse/wordcount/ejecutar_jar.sh
+++ datawarehouse/wordcount/ejecutar_jar.sh
@@ -1,9 +1,10 @@
 # Borrar dir salida en HDFS
-hadoop fs -rm /user/cloudera/out/*
-hadoop fs -rmdir /user/cloudera/out
+hadoop fs -rm /user/cloudera/out/wordcount/*
+hadoop fs -rmdir /user/cloudera/out/wordcount
 
 # Ejecutar pasando fichero/s de entrada y dir de salida
-hadoop jar wordcountmapreduce.jar org.myorg.WordCountMapReduce /user/cloudera/datawarehouse/* /user/cloudera/out 
+hadoop jar wordcountmapreduce.jar WordCountMapReduce /user/cloudera/datawarehouse/* /user/cloudera/out/wordcount 
+
 # Mostrar la salida
 hadoop fs -cat /user/cloudera/out/*
 
